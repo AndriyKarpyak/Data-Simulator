df6c23fad91c5d028c7533a16ddfaa38c68f6e63
Andriy Karpyak <kan@conscensia.com>
2015-06-12 16:26:08 +0300

    Added input argument parser.

--- data-sim/src/com/jff/dsc/DataSim.java
+++ data-sim/src/com/jff/dsc/DataSim.java
@@ -1,9 +1,18 @@
 package com.jff.dsc;
 
+import java.io.IOException;
+
 public class DataSim {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("hello");
+		System.in.read();
+		
 	}
 
+	private void start() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
 }

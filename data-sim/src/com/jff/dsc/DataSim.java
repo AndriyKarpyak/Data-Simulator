03c49975af52034fc002e8ab0ac9def35d8b71ca
Andriy Karpyak <kan@conscensia.com>
2015-06-12 16:34:01 +0300

    Removed arg parser
    Signed-off-by: Andriy Karpyak <kan@conscensia.com>

--- data-sim/src/com/jff/dsc/DataSim.java
+++ data-sim/src/com/jff/dsc/DataSim.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 public class DataSim {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("hello");
-		System.in.read();
-		
 	}
 
 	private void start() {
